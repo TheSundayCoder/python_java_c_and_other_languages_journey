92be9f5b93a1174225e3b62b311d77152cfc8360
Aditya Sinha <sinhaaditya5000@gmail.com>
2026-08-12 00:23:13 +0530

    Update 005_taking_user_input.py

---
+++ python/005_taking_user_input.py
@@ -0,0 +1,34 @@
+# a = input("enter a number")
+
+# print(a)  #input can be any data type....string float etc
+# print(type(a))  #str always
+
+# b = False
+# print(type(b)) #bool
+
+# #note**** in python input hamesha string me save hota haiiiii 
+
+# c = input("enter a name ")
+ 
+# print(c)
+
+# d = input()  #no message for input
+# print(d)
+
+
+#add two numbers
+a = input("enter first number")  #converts a to int right after input
+# error  b = input(enter second number)
+b = input("enter second number")
+#error print(a+3) cant add string and integer
+
+#mistake print(a+b)  a =3 b = 5 a+b = 35
+print(int(a)+3)
+print(int(a)+ int(b))
+
+# print(a+3)  #  -> ye scan me error nhi aaya bcz python dosent know konse data type ka hai a for now
+# hihihihi 
+
+
+#a = int(input("enter a number"))
+#directly converted into int after input is given

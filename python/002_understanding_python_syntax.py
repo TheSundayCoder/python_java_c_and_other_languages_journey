add99dc3d391599e0bf849d53331845a917a97d1
Aditya Sinha <sinhaaditya5000@gmail.com>
2026-08-11 23:08:39 +0530

    Add files via upload

---
+++ python/002_understanding_python_syntax.py
@@ -0,0 +1,11 @@
+print("hello adi")
+print("i am good")
+print("how are you")
+print(2)
+
+
+
+# if(a>3):
+#     statement 1 inside if statement
+#     statement2 (also inside)
+# statement 3 outside if statement

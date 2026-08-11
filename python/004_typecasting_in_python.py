9c31fc1414de44917089690806b0a83ee0f291d9
Aditya Sinha <sinhaaditya5000@gmail.com>
2026-08-11 23:51:54 +0530

    Add files via upload

---
+++ python/004_typecasting_in_python.py
@@ -0,0 +1,19 @@
+a =34      #int
+b = "34"  #string
+d = 223
+print(a)
+print(type(a))
+print(b)
+print(type
+      (b))
+
+#convert string b to an integer
+c = int(b)
+print(type(c))
+print(c)
+
+#convert int to string
+e = str(d)
+print(e)
+print(type(e))
+

d5fba7257991ab60c55d22476121fb95620afa4b
Aditya Sinha <sinhaaditya5000@gmail.com>
2026-08-13 11:11:26 +0530

    Add files via upload

---
+++ python/010_Match_case_Statements_in_python.py
@@ -0,0 +1,15 @@
+a = int(input())
+
+match(a) :
+    case 122:
+        print("value is 122")
+    case(3) : 
+        print("value is 3")
+    case(6) :
+        print("the value is 6")    
+    case _: #default case    
+         print("better luck next time")
+
+
+
+

677bfba1691744d0e433b91be2eaa2f770d6c03f
Aditya Sinha <sinhaaditya5000@gmail.com>
2026-08-13 10:59:56 +0530

    Add files via upload

---
+++ python/009_if_else_conditional_statement.py
@@ -0,0 +1,9 @@
+a= int(input("enter age\n"))
+if(a>18):
+    print("you can drive")
+elif(a==18):
+    print("become learner")     #indentation spaces...    
+else:
+    print("you cant drive")
+
+print("end of program")  #no indentation space...this print is outside if elif or else statement

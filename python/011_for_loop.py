0c6e725cc4915f04c4dbff32bc0c79e0a8d4a083
Aditya Sinha <sinhaaditya5000@gmail.com>
2026-08-13 11:25:02 +0530

    Add files via upload

---
+++ python/011_for_loop.py
@@ -0,0 +1,7 @@
+for i in range(1,6):      #range function goes from 1 to 7 in this case
+   print(i,end = " ")     #prints 1 to 5 
+print("\ntable of 5:")
+for i  in range(1, 11):
+     print("5*",i," is : ",5*i)
+
+     

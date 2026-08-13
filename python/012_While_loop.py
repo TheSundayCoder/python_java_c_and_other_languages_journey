2097a0605847886eb35c578fa766ba322f1f141e
Aditya Sinha <sinhaaditya5000@gmail.com>
2026-08-13 11:33:30 +0530

    Add files via upload

---
+++ python/012_While_loop.py
@@ -0,0 +1,12 @@
+i = 1
+while i<6:
+    print(i)
+    i = i +1
+
+'''
+infinite loop
+
+i = 1
+while i < 5:    (#or while true)
+      print(i)     - > infinite loop cuz i will be 1 always ..and 1 < 5
+'''

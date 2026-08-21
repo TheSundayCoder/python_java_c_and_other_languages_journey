3a2c8e93723270d02b9d9bd10e6db7fd619be1f7
Aditya Sinha <sinhaaditya5000@gmail.com>
2026-08-21 12:59:41 +0530

    Enhance string examples and indexing explanation
    Added examples of string indexing and multiline strings in Python.

--- python/015_strings_in_python.py
+++ python/015_strings_in_python.py
@@ -1,1 +1,44 @@
+# name = "HArry"
+# print(name)
 
+# name1 = 'harrry'   #cant create multiline string using '' and ""
+# name2 ='''yooo
+
+
+
+# oooo''' #multiline string
+# print(name2)
+
+# name3 = """ yoooooo
+
+# prtoert
+# trtert  """
+# print(name3)
+
+
+#string indexinggggg*********
+
+namee = "Aditya"
+        #012345
+
+
+#  A   D   I  T   Y   A
+#  0   1   2  3   4   5
+# -6  -5  -4 -3  -2  -1
+
+print(namee[0]) #output A...counting in programming starts from 0
+
+print(namee[1])  #d
+print(namee[2])  #i
+print(namee[3])  #t
+print(namee[4])  #y
+print(namee[5])  #a
+#error -> print(namee[6]) or print index goes from 0 to 5 
+print("***************")
+print(namee[-1]) #namee[5]
+print(namee[-2]) #namee[4]
+print(namee[-3]) #namee[3]
+print(namee[-4]) #namee[2]
+print(namee[-5]) #namee[1]
+print(namee[-6]) #namee[0]
+#string length (start from 1) = n then name[-y] = name[n+(-y)] in positive
